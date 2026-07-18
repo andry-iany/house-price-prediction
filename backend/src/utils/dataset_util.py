08c2df7661709c569f5cf1bb3a677a234b5e6618
Andry <andrysitrakiniainasoa@gmail.com>
2026-07-18 06:55:19 +0300

    perform eda

--- backend/src/utils/dataset_util.py
+++ backend/src/utils/dataset_util.py
@@ -11,6 +11,8 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 PATH_DATASET_DIR = PROJECT_ROOT / 'data'
 PATH_RAW_DATASET_FILE = PATH_DATASET_DIR / 'Housing.csv'
+PATH_RAW_TRAINING_DATASET = PATH_DATASET_DIR / 'Training__Housing.csv'
+PATH_RAW_TESTING_DATASET = PATH_DATASET_DIR / 'Testing_Housing.csv'
 PATH_CLEANED_DATASET_FILE = PATH_DATASET_DIR / 'Cleaned__Housing.csv'
 
 
